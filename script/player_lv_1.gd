67cc1d49682ec0a4119121a91c4bfb9b3d7d2615
justerbass <jkttzars@gmail.com>
2023-11-16 17:48:52 -0300

    main character whit movement and animation.

--- script/player_lv_1.gd
+++ script/player_lv_1.gd
@@ -1,21 +1,46 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 30
 
 
 func _physics_process(delta):
-
+	
+	## movimento del personaje
 	var directions = Input.get_axis("ui_up", "ui_down")
 	if directions:
 		velocity.y = directions * SPEED
 	else:
 		velocity.y = move_toward(0, 0, SPEED)
-
+		
+		
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(0, 0, SPEED)
-
+		
+		
+	## animacion del personaje
+	
+	if velocity.length() != 0:
+		velocity = velocity.normalized() * SPEED
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	## animacion segun direccion de movimiento
+		
+	if velocity.x > 0:
+		$AnimatedSprite2D.animation = "right"
+	elif velocity.x < 0:
+		$AnimatedSprite2D.animation = "left"
+	
+	if velocity.y > 0:
+		$AnimatedSprite2D.animation = "down"
+	elif velocity.y < 0:
+		$AnimatedSprite2D.animation = "up"
+		
+	
 	move_and_slide()
