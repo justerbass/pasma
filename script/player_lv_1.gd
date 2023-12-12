b0b74204b4f8cb1428ed3ae5ff56923143dc804d
justerbass <jkttzars@gmail.com>
2023-12-12 20:15:47 -0300

    buttons working, finally

--- script/player_lv_1.gd
+++ script/player_lv_1.gd
@@ -91,7 +91,7 @@ func _on_area_2d_body_entered(body):
 		$"../UIControl/CanvasLayer/speak_btn".show()
 	if body is guard:
 		guard_in_range = true
-		$"..w/UIControl/CanvasLayer/speak_btn".show()
+		$"../UIControl/CanvasLayer/speak_btn".show()
 	if body is ballplayer:
 		ballplayer_in_range = true
 		$"../UIControl/CanvasLayer/speak_btn".show()
--- script/ui_control.gd
+++ script/ui_control.gd
@@ -1,11 +1,40 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if global.player_aplause_learned:
+		$CanvasLayer/aplause_btn.show()
+		
+	if global.player_peace_learned:
+		$CanvasLayer/peace_btn.show()
+
+func _on_peace_btn_pressed():
 	pass
+
+
+func _on_aplause_btn_pressed():
+	pass
+
+
+func _on_speak_btn_pressed():
+	$CanvasLayer/speak_btn.hide()
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
+
+
+func _on_right_btn_pressed():
+	pass
+
+
+func _on_left_btn_pressed():
+	pass
+
+
+func _on_down_btn_pressed():
+	pass
+
+
+func _on_up_btn_pressed():
+	pass
+
