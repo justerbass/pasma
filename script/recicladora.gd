dce94e218d8662571ab4ed0df870558f92431da0
justerbass <jkttzars@gmail.com>
2024-01-24 00:58:19 -0300

    plastic bridge ready

--- script/recicladora.gd
+++ script/recicladora.gd
@@ -15,28 +15,65 @@ func paper_recycled():
 	if Input.is_action_just_pressed("TAKE"):
 		$"../UIControl/CanvasLayer/take_btn".hide()
 		
-		if global.paper_count > 10:
-			#ciclo de animacion de entregar papel
-			while global.paper_count > 0:
+		##requisitos nivel plastico
+		if global.plastic_count > 13 and global.paper_count > 10:
+			#ciclo de animacion de entrega de materiales
+			while global.plastic_count > 0:
 				$"../PlayerLv2/AnimatedSprite2D".play("deliver")
 				await get_tree().create_timer(1).timeout
+				global.plastic_count -= 1
+				if global.plastic_count < 0:
+					global.plastic_count = 0
 				global.paper_count -= 1
 				if global.paper_count < 0:
 					global.paper_count = 0
-				
-			#hacer funcionar la maquina
-			if global.paper_count == 0:
+			if global.plastic_count == 0 and global.paper_count == 0:
 				$AnimationPlayer.play("work")
 				$"../PlayerLv2/AnimatedSprite2D".stop()
-			#construir el puente
+				#construir el puente
 				await get_tree().create_timer(3).timeout
 				$AnimationPlayer.stop()
 				$CollisionShape2D.disabled = true
 				$"baliza azul".hide()
 				$"baliza roja".show()
-				$"../puente papel/AnimationPlayer".play("build")
+				$"../puente plastico/AnimationPlasticBridge".play("build")
 				$"../PlayerLv2/AnimationCameraPlayer".play("bridge")
-				
+				await  get_tree().create_timer(30).timeout
+				$CollisionShape2D.disabled = false
+				$"baliza azul".show()
+				$"baliza roja".hide()
+		
 		else:
-			DialogueManager.show_example_dialogue_balloon(load("res://script/recycler.dialogue"), "start_recycler")
-			return
+			##requisitos nivel papel
+			if global.paper_count > 10:
+				#ciclo de animacion de entregar papel
+				while global.paper_count > 0:
+					$"../PlayerLv2/AnimatedSprite2D".play("deliver")
+					await get_tree().create_timer(1).timeout
+					global.paper_count -= 1
+					if global.paper_count < 0:
+						global.paper_count = 0
+					
+				#hacer funcionar la maquina
+				if global.paper_count == 0:
+					$AnimationPlayer.play("work")
+					$"../PlayerLv2/AnimatedSprite2D".stop()
+				#construir el puente
+					await get_tree().create_timer(3).timeout
+					$AnimationPlayer.stop()
+					$CollisionShape2D.disabled = true
+					$"baliza azul".hide()
+					$"baliza roja".show()
+					$"../puente papel/AnimationPlayer".play("build")
+					$"../PlayerLv2/AnimationCameraPlayer".play("bridge")
+					await  get_tree().create_timer(30).timeout
+					$CollisionShape2D.disabled = false
+					$"baliza azul".show()
+					$"baliza roja".hide()
+				
+			else:
+				DialogueManager.show_example_dialogue_balloon(load("res://script/recycler.dialogue"), "start_recycler")
+				$CollisionShape2D.disabled = true
+				await  get_tree().create_timer(3).timeout
+				$CollisionShape2D.disabled = false
+				return
